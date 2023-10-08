fbf89b90b0700d58f054f2edb9690795b3240588
chu0802 <r09922104@ntu.edu.tw>
2023-10-09 00:32:07 +0800

    <chore>: formatting

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,25 +1,24 @@
-import requests
 import os
 
-base_url = 'http://127.0.0.1:8000'  # Replace with your actual URL
+import requests
+
+base_url = "http://127.0.0.1:8000"  # Replace with your actual URL
 test_user_name = "bubu"
 TOKEN = os.environ[f"{test_user_name.upper()}_TOKEN"]
 
+
 def test_register_user():
     response = requests.post(
-        f"{base_url}/register/",
-        json={"username": test_user_name},
-        headers={"Authorization": f"Bearer {TOKEN}"}
+        f"{base_url}/register/", headers={"Authorization": f"Bearer {TOKEN}"}
     )
 
     assert response.status_code == 200
     assert response.json() == {"message": "Registration successful"}
 
+
 def test_register_existing_user():
     response = requests.post(
-        f"{base_url}/register/",
-        json={"username": test_user_name},
-        headers={"Authorization": f"Bearer {TOKEN}"}
+        f"{base_url}/register/", headers={"Authorization": f"Bearer {TOKEN}"}
     )
 
     assert response.status_code == 400
